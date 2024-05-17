70a3cf17770d5bc94fcaba34fef469bca24aaa24
Nitish Kumar <raaznitish9931@gmail.com>
2024-05-18 00:30:18 +0530

    FormWithGetMethod

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-// we are getting the output using post method.
 
 const express = require('express')
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.set("view engine", "ejs")
 
-
-
-
-app.get("/", function(req, res){
+app.get("/", function(req, res){  
     res.render("index")
-})
-app.post("/profile", function(req, res){
-    res.render("index")
-    console.log(req.body)
+    console.log(req.query)
 })
 
+app.get("/profile", function(req, res){
+    res.render("index")
+})
 
-app.listen(4000);
-
-
-// const express = require('express')
-
-// const app = express();
-
-// app.set("view engine", "ejs")
-
-// app.get("/profile", function(req, res){  
-//     res.render("index")
-//     console.log(req.query)
-// })
-
-// app.get("/", function(req, res){
-//     res.send("index123")
-// })
-
-// app.listen(3000);
+app.listen(3000);
